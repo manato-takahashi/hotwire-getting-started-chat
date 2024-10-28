3e0b621575f8b3d33932b1d5b0dec9f4891d4d80
高橋真叶 <manato.takahashi86@gmail.com>
2024-10-28 16:48:43 +0900

    add: アソシエーションの定義

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_28_073342) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_28_073733) do
   create_table "entries", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "room_id", null: false
@@ -25,6 +25,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_28_073342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "evaluation", default: 0, null: false
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -46,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_28_073342) do
 
   add_foreign_key "entries", "rooms"
   add_foreign_key "entries", "users"
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
