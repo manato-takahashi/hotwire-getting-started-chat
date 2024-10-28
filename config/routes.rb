aa2b5fefe7e4dd87e07f823a9ac4ae500f5cf269
高橋真叶 <manato.takahashi86@gmail.com>
2024-10-28 16:36:07 +0900

    add: deviseのUserモデル、ビュー、コントローラーとroomsのコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'rooms/show'
+  get 'users/index'
+  get 'users/show'
+  devise_for :users
   resources :messages, only: %i(index new create edit update destroy) do
     resource :evaluation, only: %i(update), module: :messages
   end
